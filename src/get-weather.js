fd2edde59acb9c29d24d2ed549e2ef77ea0e6df9
Mostly-mous <maurice.mattle@ict.csbe.ch>
2026-02-28 13:59:23 +0100

    Revert "integration weather data, installed yarn"
    This reverts commit 37a02c0f7a888451ada5c7ffe343d17bcf0d340c.

--- src/get-weather.js
+++ src/get-weather.js
@@ -1,36 +1,38 @@
-const APIKey = "fca363e2524e2c7236dd33d4c99b12f1";
+function fetchWeatherInfo(location) {
+    const APIKey = "fca363e2524e2c7236dd33d4c99b12f1";
 
-const fetchWeatherInfo = async (location) => {
-    const geoResponse = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${APIKey}`);
+     const fetchGeo = async (location) => {
+        const geoResponse = await fetch (`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${APIKey}`);
 
-    if (!geoResponse.ok) {
-        throw new Error("An Error occured while searching for the specified location");
+        if (!geoResponse.ok) {
+            throw new Error("An Error occured while searching for the specified location");
+        }
+
+        const locData = await geoResponse.json();
+
+        if (!locData.length) {
+            throw new Error("Location not found");
+        }
+
+        const lat = locData[0].lat;
+        const lon = locData[0].lon;
+
+        const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKey}`);
+
+        if (!weatherResponse.ok) {
+            throw new Error("An Error occured while fetching weather data");
+        }
+
+        const weatherData = await weatherResponse.json();
+
+        return {
+            temp: weatherData.main.temp,
+            high: weatherData.main.temp_max,
+            low: weatherData.main.temp_min
+        }
+        
     }
-
-    const locData = await geoResponse.json();
-
-    if (!locData.length) {
-        throw new Error("Location not found");
-    }
-
-    const lat = locData[0].lat;
-    const lon = locData[0].lon;
-
-    const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKey}`);
-
-    if (!weatherResponse.ok) {
-        throw new Error("An Error occured while fetching weather data");
-    }
-
-    const weatherData = await weatherResponse.json();
-
-    return {
-        temp: weatherData.main.temp,
-        high: weatherData.main.temp_max,
-        low: weatherData.main.temp_min
-    }
-
+    return fetchGeo;
 }
-
 
 module.exports = fetchWeatherInfo;
