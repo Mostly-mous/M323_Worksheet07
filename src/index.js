ae8dde2e29d89b7dbee120334b39886155e88704
Mostly-mous <maurice.mattle@ict.csbe.ch>
2026-02-28 10:06:49 +0100

    base structure
    Created base structure
    Premade index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const view = require("./view");
+const update = require("./update");
+const app = require("./app");
+
+
+const { initModel } = require("./model");
+
+
+// The root node of the app (the div with id="app" in index.html)
+const rootNode = document.getElementById("app");
+
+// Start the app
+app(initModel, update, view, rootNode);
