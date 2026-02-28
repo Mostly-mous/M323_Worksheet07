217dea0ae0bfbc8d54ca43b30981b5bc941ba5eb
Mostly-mous <maurice.mattle@ict.csbe.ch>
2026-02-28 13:57:41 +0100

    corrected spelling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-const view = require("./view");
-const update = require("./update");
-const app = require("./app");
-
-
+const {view} = require("./view");
+const {update} = require("./update");
+const {app} = require("./app");
 const { initModel } = require("./model");
 
 
 // The root node of the app (the div with id="app" in index.html)
-const rootNode = document.getElementById("app");
+const {rootNode} = document.getElementById("app");
 
 // Start the app
 app(initModel, update, view, rootNode);
